e24f06ea2edf5d8a3da158691258745a96b1ff00
RoyDavinci <56495698+RoyDavinci@users.noreply.github.com>
2020-06-06 21:15:24 +0100

    Create hng-id-04356.py
    created a variable and stored my values in a dictionary, used the print statement to show values.

---
+++ hng-id-04356.py
@@ -0,0 +1,8 @@
+profile = {
+    "name" : "Mathias Chukwudi",
+    "user_id" : "HNG-04356",
+    "language" : "python",
+    "email" : "emsthias33@gmail.com"
+    }
+
+print("Hello World, this is " + profile['name'] + " with HNGi7-ID " + profile['user_id'] +  ' using ' + profile['language'] + ' for stage 2 task.')
