ff257ad216c1288ccb6a4d5333d2ca0d8a32d5a7
RoyDavinci <56495698+RoyDavinci@users.noreply.github.com>
2020-06-06 09:24:35 +0100

    roy.py
    I created a variable called profile, declared it as a dictionary and used the print statement to print out its details.

---
+++ scripts/roy.py
@@ -0,0 +1,8 @@
+profile = {
+    "name" : "Mathias Chukwudi",
+    "user_id" : "HNG-04356",
+    "language" : "python",
+    "email" : "emsthias33@gmail.com"
+    }
+
+print("Hello World, this is " + profile['name'] + "with HNG-ID " + profile['user_id'] + 'using ' + profile['language'] + ' for stage 2 task.')
